f17505a4ef2d06276e6b768ad2ec59267a2dfc98
swelch940 <95835442+swelch940@users.noreply.github.com>
2023-03-09 09:09:17 +0000

    Adding the project.

---
+++ Project/maze-aStar.py
@@ -0,0 +1,90 @@
+import time
+from queue import PriorityQueue
+start_time = time.time()
+
+maze = [] #Intializing maze.
+
+input_file = input("Enter maze file: ")
+input_start = input("Enter the location of the start of the maze: ")
+input_end = input ("Enter the location of the end of the maze: ")
+
+input_start =  eval(input_start)
+input_end = eval(input_end)
+
+#Populating maze as 2d array from text file given.
+with open(input_file, "r") as file:
+    for row in file:
+        maze.append(row.strip().split())
+
+#Function that returns a list of all the possible legal moves at a certain point in the maze.
+def legalMoves(points, maze):
+    nextPoints = []
+    if points[0] - 1 >= 0 and maze[points[0] - 1][points[1]] != "#":
+        nextPoints.append((points[0] - 1, points[1]))
+    if points[1] + 1 < len(maze[points[0]]) and maze[points[0] + 1][points[1]] != "#":
+        nextPoints.append((points[0] + 1, points[1]))
+    if points[1] + 1 < len(maze[points[0]]) and maze[points[0]][points[1] + 1] != "#":
+        nextPoints.append((points[0], points[1] + 1))
+    if points[1] - 1 >= 0 and maze[points[0]][points[1] - 1] != "#":
+        nextPoints.append((points[0], points[1] - 1))
+    
+    
+    return nextPoints
+
+
+#Heuristic function to calculate the manhattan distance for a point. 
+#Using the manhattan distance as the heuristic in this algorithm.
+def heuristic(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return abs(x1- x2) + abs(y1 -y2) #Returns manhattan distance for a given point on the graph. 
+
+
+#A* search to find the most opitmal path through the maze.
+def aStar(maze, start, goal):
+    g_node = {}
+    f_node ={}
+    visited = set()
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            g_node[(i,j)] = float('infinity')
+    g_node[start]= 0
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            f_node[(i,j)] = float('infinity')
+    f_node[start] = heuristic(start, goal)
+
+    queue = PriorityQueue()
+    queue.put((heuristic(start,goal), heuristic(start,goal), start))
+    path = {}
+    while queue:
+        currentPoint = queue.get()[2]
+        if currentPoint == goal:
+            print(str(currentPoint) + " is the exit!")
+            print("Number of visted nodes: " +str(len(visited)))
+            end_time = time.time()
+            total_time = end_time- start_time
+            print("Excecution time: " + str(total_time))
+            fwdPath = {}
+            cell = goal
+            while cell != start:
+                fwdPath[path[cell]] = cell
+                cell = path[cell]
+            print("Total number of steps in path: " + str(len(fwdPath)))
+            return
+        
+        nextPoints = legalMoves(currentPoint, maze)
+        for i in nextPoints:
+            temp_g_node = g_node[currentPoint] + 1
+            temp_f_node = temp_g_node + heuristic(i, goal)
+
+            if temp_f_node < f_node[i]:
+                g_node[i] = temp_g_node
+                f_node[i] = temp_f_node
+                queue.put((temp_f_node, heuristic(i, goal), i))
+                path[i] = currentPoint
+                visited.add(i)
+    
+    
+
+aStar(maze, input_start, input_end)
---
+++ Project/maze-solver.py
@@ -0,0 +1,93 @@
+import numpy as np
+import time
+
+start_time = time.time() #Gets the start time of the algorithm
+
+maze = [] #Intialising maze.
+
+input_file = input("Enter maze file: ")
+input_start = input("Enter the location of the start of the maze: ")
+input_end = input ("Enter the location of the end of the maze: ")
+
+input_start =  eval(input_start)
+input_end = eval(input_end)
+
+#Popluating maze as a 2d array from the text file given.
+with open(input_file, "r") as file:
+    for row in file:
+        maze.append(row.strip().split())
+
+
+#Depth-first search algorithm to calculate a path through a given matrix.
+def dfs(maze, start, goal, stack, seen):
+    seen = set() #Set of possible positions in the maze we have seen. 
+    stack = [start] #A stack to keep track of the positions in the maze wee need to evaluate.
+    parentMap = {} #Hash map to keep track of the path we take through the maze.
+    
+
+    #While loop that make sure we keep going until there are no more possible moves in the stack.
+    while stack:
+        print("\n")
+        currentPoint = stack.pop() #Gets a position to evaluate.
+        #print(currentPoint)
+        #print("Looking at " + str(currentPoint))
+        #print("Is " + str(currentPoint) + " the exit?")
+
+        #If the current point we are at ion the maze is the exit, we are finished.
+        if currentPoint == goal:
+            print(str(currentPoint) + " is the exit!")
+            end_time = time.time()
+            total_time = end_time- start_time
+            print("Excecution time: " + str(total_time))
+            print("Total nodes visited: " + str(len(seen)))
+            path = []
+            while currentPoint != start:
+                path.append(currentPoint)
+                currentPoint = parentMap.get(currentPoint)
+                
+            #print("Path: " + str(path))
+            print("Total number of steps in path: " + str(len(path)))
+            return
+        print("No!")
+        
+
+        #Gets the list of all the possible legal moves for the current point in the maze.
+        nextPoints = legalMoves(currentPoint, maze)
+        #print("Possible moves: " + str(nextPoints))
+        
+        #Goes through the list of all the possible moves and adds them to the list of seen and the stack.
+        #Also adds them as the key to their parent node. Meaning the previous move to get to that 
+        for i in nextPoints:
+            if i in seen:
+                #print(" Already seen " + str(i))
+                continue
+            seen.add(i)
+            parentMap[i] = currentPoint
+            #print("Visted nodes: " + str(seen))
+            stack.append(i)
+            #print("List of nodes to visit: " + str(stack))
+
+
+#Function that returns a list of all the possible legal moves at a certain point in the maze.
+def legalMoves(points, maze):
+    nextPoints = []
+    if points[0] - 1 >= 0 and maze[points[0] - 1][points[1]] != "#":
+        nextPoints.append((points[0] - 1, points[1]))
+    if points[1] + 1 < len(maze[points[0]]) and maze[points[0] + 1][points[1]] != "#":
+        nextPoints.append((points[0] + 1, points[1]))
+    if points[1] + 1 < len(maze[points[0]]) and maze[points[0]][points[1] + 1] != "#":
+        nextPoints.append((points[0], points[1] + 1))
+    if points[1] - 1 >= 0 and maze[points[0]][points[1] - 1] != "#":
+        nextPoints.append((points[0], points[1] - 1))
+    
+    
+    return nextPoints
+
+
+
+
+stack = []
+seen = set()
+dfs(maze, input_start, input_end, stack, seen)
+
+
